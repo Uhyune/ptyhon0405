6b1d497ee4fb6444974896f3a41e5513c493f8bc
Uhyune <annone@hanmail.net>
2021-04-05 11:50:26 +0900

    두번째 버전

--- web1.py
+++ web1.py
@@ -10,4 +10,16 @@ soup = BeautifulSoup(page,"html.parser")
 #<p>를 모두 검색 리스트 형식 리턴
 # print(soup.find_all("p"))
 #첫번째<p>를 검색
-print(soup.find("p"))
+# print(soup.find("p"))
+#<p class="outer-text"> 된 경우 (약간의 필터링)
+#print(soup.find_all("p",class_="outer-text")) #class는 예약어라 _붙여서 피함
+#특정 id 속성을 검색
+#print(soup.find(id="first"))
+#<a>태그 가져오기
+#print(soup.find_all("b"))
+
+#태그를 제거하고 내부 문자열(컨텐츠만)
+#<p>컨텐츠</p>
+for tag in soup.find_all("p"):
+    #앞 뒤에 공백문자 제거
+    print(tag.text.strip())
