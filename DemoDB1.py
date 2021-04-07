8f32e9f01a61ecb0923ee74f683162efdc007cb4
Uhyune <annone@hanmail.net>
2021-04-07 12:50:15 +0900

    레코드 출력

--- DemoDB1.py
+++ DemoDB1.py
@@ -21,5 +21,24 @@ cur.executemany("insert into PhoneBook values (?, ?);",datalist)
 
 #검색
 cur.execute("select * from PhoneBook;")
-for row in cur:
-    print(row)
+# for row in cur:
+#     print(row)
+#1건을 검색
+print( cur.fetchone())
+#4건을 검색
+print("===fetchmany(2)===")
+print(cur.fetchmany(2))
+print("===fetchAll()===")
+cur.execute("select * from PhoneBook;") #검색구문
+print(cur.fetchall())
+
+# 결과를 슬라이싱
+cur.execute("select * from PhoneBook;")
+result = cur.fetchone()
+print(result[0])
+print(result[1])
+#2차원 행열 데이터 [행][열]
+print ("===다중행의 경우 ===")
+result = cur.fetchall()
+print(result[0][0])
+print(result[0][1])
